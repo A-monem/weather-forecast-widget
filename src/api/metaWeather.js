4d55c94043a951fa84cd4e7e08536aa7647ebf33
Abdelmoneim Nafea <aamnafea@hotmail.com>
2021-02-27 00:56:33 +1100

    add metaWeather file

---
+++ src/api/metaWeather.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+// I am using a node server deployed on heroku to perform the api calls to MetaWeather in order to
+// bypass CORS issue
+
+const getWeather = (id) => new Promise((resolve, reject) => {
+  try {
+    axios.get(`https://limitless-dusk-13082.herokuapp.com/api/weather/${id}`)
+      .then((response) => {
+        const weatherInfo = response.data.consolidated_weather.map((dayWeatherInfo) => ({
+          maxTemp: dayWeatherInfo.max_temp,
+          minTemp: dayWeatherInfo.min_temp,
+          state: dayWeatherInfo.weather_state_name,
+          stateAbbr: dayWeatherInfo.weather_state_abbr,
+          windSpeed: dayWeatherInfo.wind_speed,
+        }));
+
+        weatherInfo.shift();
+        weatherInfo.pop();
+
+        resolve(weatherInfo);
+      });
+  } catch (error) {
+    reject(error);
+  }
+});
+
+const getLocation = (location) => new Promise((resolve, reject) => {
+  try {
+    axios.get(`https://limitless-dusk-13082.herokuapp.com/api/location/${location}`)
+      .then((response) => {
+        const locationsList = response.data.map((loc) => ({ title: loc.title, id: loc.woeid }));
+
+        resolve(locationsList);
+      });
+  } catch (error) {
+    reject(error);
+  }
+});
+
+export { getWeather, getLocation };
